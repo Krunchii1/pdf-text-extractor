2949ce424a34c20bc930a3b9aa47f2bf3c334fa5
Krunchii1 <chuaj.niel48@gmail.com>
2024-04-03 23:39:12 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# pdf-text-extractor
+# PDF Text Extractor Commission
+
+This program scans all available articles (.pdf) within the directory and extracts all the text into a string. It then counts the occurrences of each keyword found in `keywords.txt`. The results are then placed in `results.csv` containing the article's ID along with the number of times each keyword appeared in the article.
+
+## Setup
+
+1. Clone the GitHub repository by going to `cmd` and typing the command in your desired directory: 
+`git clone https://github.com/Krunchii1/pdf-text-extractor.git`
+2. Install `PyPDF2` through `pip` by typing the command: `pip install PyPDF2` in the main directory of the program.
+3. Edit `keywords.txt` to place all the keywords that you want to check for.
+4. Create a folder to insert all your PDFs in. The program should be able to find the PDF regardless of how the directory is organized.
+5. Run the program! The results will be saved in `results.csv`.
+
+#### > Commission by Josh C.
---
+++ scanner.py
@@ -0,0 +1,49 @@
+from glob import glob
+from pathlib import Path
+import os
+import argparse
+import PyPDF2
+import re
+import csv
+
+def extract_keywords(filename):
+    with open(filename) as file:
+        lines = [line.rstrip() for line in file]
+    return lines
+
+def count_all_occurrences(pdf_str, keywords):
+    counts = {keyword: 0 for keyword in keywords}
+    for keyword in keywords:
+        if keyword in pdf_str:
+            counts[keyword] += 1
+    return counts
+
+def extract_text_from_pdf(pdf_file):
+    with open(pdf_file, 'rb') as pdf:
+        reader = PyPDF2.PdfReader(pdf, strict=False)
+        text = []
+
+        for page in reader.pages:
+            content = page.extract_text()
+            text.append(content)
+
+    return text
+    
+if __name__ == '__main__':
+    pdfs = []
+    for path in Path('./').rglob('*'):
+        if path.suffix.lower() == ".pdf":
+            pdfs.append(path)
+
+    keywords = extract_keywords('keywords.txt')
+
+    with open('result.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        field = ['article_id'] + [keyword for keyword in keywords]
+        writer.writerow(field)
+        for pdf in pdfs:
+            extracted_text = extract_text_from_pdf(pdf)[0].replace('\n', ' ')
+            counts = count_all_occurrences(extracted_text, keywords)
+            filename = str(pdf)[str(pdf).rindex("\\") + 1:len(str(pdf))-4]
+            row = [filename] + list(counts.values())
+            writer.writerow(row)

026ebfe0b0923ea2b18d817fb47a38b9534fecf3
Krunchii1 <chuaj.niel48@gmail.com>
2024-04-16 19:47:41 +0800

    Skips corrupt PDFs

--- scanner.py
+++ scanner.py
@@ -13,23 +13,35 @@ def extract_keywords(filename):
 
 def count_all_occurrences(pdf_str, keywords):
     counts = {keyword: 0 for keyword in keywords}
-    print(pdf_str.lower())
     for keyword in keywords:
         counts[keyword] = pdf_str.lower().count(keyword)
     return counts
 
 def extract_text_from_pdf(pdf_file):
-    with open(pdf_file, 'rb') as pdf:
-        reader = PyPDF2.PdfReader(pdf, strict=False)
-        text = []
+    if not os.path.exists(pdf_file):
+        print("Error: File does not exist")
+        return None
+    
+    if os.path.getsize(pdf_file) == 0:
+        print("Error: File is empty")
+        return 1
+    
+    try:
+        with open(pdf_file, 'rb') as pdf:
+            reader = PyPDF2.PdfReader(pdf, strict=False)
+            text = []
 
-        for page in reader.pages:
-            text.append(page.extract_text())
+            for page in reader.pages:
+                text.append(page.extract_text())
 
-    return text
+        return text
+    except Exception as e:
+        print("Error:", e)
+        return None
     
 if __name__ == '__main__':
     pdfs = []
+    errors = []
     for path in Path('./').rglob('*'):
         if path.suffix.lower() == ".pdf":
             pdfs.append(path)
@@ -42,9 +54,14 @@ if __name__ == '__main__':
         writer.writerow(field)
         for pdf in pdfs:
             extracted_text = extract_text_from_pdf(pdf)
+            if extracted_text == 1:
+                errors.append(pdf)
+                continue
             extracted_text = ' '.join(extracted_text).replace('\n', ' ')
             counts = count_all_occurrences(extracted_text, keywords)
             filename = str(pdf)[str(pdf).rindex("\\") + 1:len(str(pdf))-4]
             print('Scanning:', pdf)
             row = [pdf] + list(counts.values())
             writer.writerow(row)
+
+    print(errors)
